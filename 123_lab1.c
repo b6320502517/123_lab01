e7485c1d4a8fe6967d8ef9bf5920fb572b0a0d37
b6320502517 <sarannut.l@ku.th>
2021-02-02 00:10:03 +0700

    Create 123_lab1.c

---
+++ 123_lab1.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char x[30];
+    scanf("%[^\n]s",x);
+    printf("%s",x);
+
+    return 0;
+}
